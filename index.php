0344409c736f37a3091197594450e9d57ee60acb
Suben Bigwa <bigwasuben@gmail.com>
2023-03-09 20:46:19 +0100

    commit

--- index.php
+++ index.php
@@ -14,15 +14,11 @@
         <link rel="stylesheet" href="fonts/css/all.min.css"> 
         <script src="https://kit.fontawesome.com/3dc7f12c51.js" crossorigin="anonymous"></script>
 
-        <!--BOOTSTRAP  -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <!-- Automatic Card slider -->
+        <script src="js/lib/jquery-3.5.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-
-   
         <!-- Amination Scroll-->
         <!-- <link rel="stylesheet" href="aos-by-red.css"> -->
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
@@ -44,9 +40,6 @@
         <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet" href="css/stylesheet.css">
         <link rel="stylesheet" href="css/responsive.css">
-        <!-- <link rel="stylesheet" href="/css/cropcss.css">
-    <link rel="stylesheet" href="/css/buttonstyle.css"> -->
-
 
         <!-- MODERNIZR LIBRARY -->
         <script src="js/modernizr-custom.js"></script>
@@ -75,13 +68,16 @@
 <body>
 
 
-
-
 <!-- TEST COMPONENT STARTS HERE  -->
 
  
 
-<?php require("components/zowaseltestimonial.php");   ?>
+<?php# require("components/zowaseltestimonial.php");   ?>
+<?php# require('components/report.php'); ?>
+<?php# require('components/safety.php'); ?>
+<?php  require('components/traceabilityoverview.php'); ?>
+<?php  require('components/traceabilityprojects.php'); ?>
+<?php  require('components/traceabilityservices.php'); ?>
 
 
  
@@ -92,8 +88,12 @@
 
 
 
-  <!-- JAVASCRIPTS -->
-    <script src='js/lib/jquery-3.5.1.min.js'></script>
+
+
+
+
+ <!-- JAVASCRIPTS -->
+ <script src='js/lib/jquery-3.5.1.min.js'></script>
     <script src='js/lib/plugins.js'></script>
     <script src='js/lib/bootstrap.min.js'></script>
     <!-- <script src='js/lib/flickity.pkgd.min.js'></script> -->
